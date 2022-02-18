ed2980e727f67e7fe0df675c11c37771ab561122
Aveturi13 <advaithveturi@gmail.com>
2022-02-18 09:47:36 +0000

    - Add VGG11 and AlexNet architectures - Set image size and channels as global vars

--- eye2gene_retraining/retrain.py
+++ eye2gene_retraining/retrain.py
@@ -52,6 +52,7 @@ CRITERION = nn.CrossEntropyLoss()
 OPTIMIZER = lambda x, y, z: optim.Adam(x, lr=y, weight_decay=z)
 N_CLASSES = 36
 IM_SIZE = 299
+N_CHANNELS = 3
 
 def save_config(args):
     with open(os.path.join(args.save_dir, "model_config.json"), 'w') as f:
@@ -75,8 +76,8 @@ def load_data(train_fpath, val_fpath, **kwargs):
         val_transforms.append(transforms.Resize((IM_SIZE, IM_SIZE)))
 
     # convert to grayscale - compulsory
-    train_transforms.append(transforms.Grayscale(3))
-    val_transforms.append(transforms.Grayscale(3))
+    train_transforms.append(transforms.Grayscale(N_CHANNELS))
+    val_transforms.append(transforms.Grayscale(N_CHANNELS))
 
     # random hflip - optional, but better for augmentation purposes 
     if "random_hflip" in kwargs:
@@ -130,9 +131,15 @@ def load_model(name="inceptionv3", device='cpu'):
     elif name == "vgg16":
         model = torchvision.models.vgg16_bn(pretrained=True)
         model.classifier[6] = nn.Linear(4096, N_CLASSES)
+    elif name == "vgg11":
+        model = torchvision.models.vgg11_bn(pretrained=True)
+        model.classifier[6] = nn.Linear(4096, N_CLASSES)
+    elif name == "alexnet":
+        model = torchvision.models.alexnet(pretrained=True)
+        model.classifier[6] = nn.Linear(4096, N_CLASSES)
     elif name == "simple":
         from custom_models import multiClassPerceptron
-        model = multiClassPerceptron(in_channels=3*299*299, hidden_layers=[], out_channels=N_CLASSES)
+        model = multiClassPerceptron(in_channels=N_CHANNELS*IM_SIZE*IM_SIZE, hidden_layers=[], out_channels=N_CLASSES)
     else:
         raise ValueError("Model can only be `inceptionv3`, `vgg16` or `resnet18`")
 
--- eye2gene_retraining/retrain_eye2gene.sh
+++ eye2gene_retraining/retrain_eye2gene.sh
@@ -1,4 +1,4 @@
-TRAIN_DIR=/home/zchayav/projects/syntheye/synthetic_datasets/dummy_synthetic_100/generated_examples.csv
+TRAIN_DIR=/home/zchayav/projects/syntheye/datasets/eye2gene_new_filepaths/all_baf_valid_50deg_filtered_train_0_edited.csv
 VAL_DIR=/home/zchayav/projects/syntheye/datasets/eye2gene_new_filepaths/all_baf_valid_50deg_filtered_val_0_edited.csv
-SAVE_DIR=noise_3600_only_again
+SAVE_DIR=one_fold_retraining/noise_3600_only_again
 python retrain.py --model=inceptionv3 --train=$TRAIN_DIR --val=$VAL_DIR --epochs=100 --resize=299 --batch-size=128 --lr=0.0001 --weight-decay=0 --save-best=1 --w-monitor=acc --es=1 --es-monitor=loss --save-dir=$SAVE_DIR --save-logs --device=3 --verbose --seed=1502
