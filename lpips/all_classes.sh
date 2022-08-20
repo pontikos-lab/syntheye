3a4e5a747a90a4d1abf54d8cf5d3e877fd0239da
Aveturi13 <advaithveturi@gmail.com>
2022-08-20 10:16:28 +0100

    Remove lpips scores

--- lpips/all_classes.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-path1=/home/zchayav/projects/syntheye/datasets/eye2gene_new_filepaths/all_baf_valid_50deg_filtered_train_0_edited.csv
-path2=$path1
-while read line; do python ./lpips/compare_pairs.py -p0=$path1 -p1=$path2 -c=$line -o=lpips_$line.csv --use_gpu; done < classes.txt
